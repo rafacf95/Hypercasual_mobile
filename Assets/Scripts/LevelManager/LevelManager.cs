fe0e5b19aa8b8707dffcfa9130bb692eace6b111
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-17 16:01:35 -0300

    Updated LevelManager.cs

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -7,9 +7,18 @@ public class LevelManager : MonoBehaviour
     public Transform container;
     public List<GameObject> levels;
 
-    [SerializeField] private int _index;
-    private GameObject _currentLevel;
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPeieces;
+    public LevelPieceBase endPiece;
+    public int piecesNumber = 5;
+    public float timeBetweenPieces = .3f;
 
+    private int _index;
+    private GameObject _currentLevel;
+    private List<LevelPieceBase> _spawnedPieces;
+    private LevelPieceBase _lastPiece;
+
+    #region Level
     private void SpawnNextLevel()
     {
 
@@ -27,14 +36,55 @@ public class LevelManager : MonoBehaviour
         _currentLevel = Instantiate(levels[_index], container);
         _currentLevel.transform.localPosition = Vector3.zero;
     }
-
     private void ResetLevelindex()
     {
         _index = 0;
     }
+    #endregion
+
+    #region Pieces
+
+    private void CreateLevel()
+    {
+        // StartCoroutine(CreateLevelPiecesCoroutine());
+        _spawnedPieces = new List<LevelPieceBase>();
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece();
+        }
+        var end = Instantiate(endPiece, container);
+        end.transform.position = _lastPiece.endPosition.position;
+    }
+    private void CreateLevelPiece()
+    {
+        var piece = levelPeieces[Random.Range(0, levelPeieces.Count)];
+        var spawned = Instantiate(piece, container);
+
+        if (_spawnedPieces.Count > 0)
+        {
+            _lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+            spawned.transform.position = _lastPiece.endPosition.position;
+        }
+
+        _spawnedPieces.Add(spawned);
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece();
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+    }
+
+    #endregion
+
     void Awake()
     {
-        SpawnNextLevel();
+        // SpawnNextLevel();
+        CreateLevel();
     }
 
     void Update()
@@ -43,5 +93,7 @@ public class LevelManager : MonoBehaviour
         {
             SpawnNextLevel();
         }
+
+
     }
 }
