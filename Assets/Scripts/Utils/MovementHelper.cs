61fbc6a393131b747469c816458fced34e593d83
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-18 17:25:57 -0300

    Implemented MovementHelper.cs

---
+++ Assets/Scripts/Utils/MovementHelper.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHelper : MonoBehaviour
+{
+    public List<Transform> positions;
+    public float duration = 1f;
+
+    private int _index = 0;
+
+    void Start()
+    {
+        // transform.position = positions[0].transform.position;
+        transform.position = positions[RandomIndex()].transform.position;
+        NextIndex();
+    
+        StartCoroutine(StartMoviment());
+    }
+
+    private void NextIndex()
+    {
+        _index++;
+        if (_index >= positions.Count) _index = 0;
+    }
+
+    private int RandomIndex()
+    {
+        return Random.Range(0, positions.Count);
+    }
+
+    IEnumerator StartMoviment()
+    {
+        float time = 0;
+
+        while (true)
+        {
+
+            var curretnPosition = transform.position;
+
+            while (time < duration)
+            {
+                transform.position = Vector3.Lerp(curretnPosition, positions[_index].transform.position, (time / duration));
+
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            NextIndex();
+            time = 0;
+
+            yield return null;
+        }
+    }
+}
